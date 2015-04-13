0868a7e1ff1b5d83734c5703d098c4d7c79e3aca
Jules Rogmans <websitewizardguy@gmail.com>
2015-04-13 00:49:46 -0700

    ADD UPDATE AND DELETE BUTTONS
    SQL database currently sets section_id to 1 and changes back to 1 when
    updated. Planning on adding better functionality to that feature later

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,9 +6,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
@@ -18,9 +30,23 @@ class ItemsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to sections_index_path, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ Rails.application.routes.draw do
 
   get 'sections/show'
 
-  get 'items/index'
+  # get 'items/index'
 
-  get 'items/new'
+  # get 'items/new'
 
-  get 'items/create'
+  # get 'items/create'
 
-  get 'items/show'
+  # get 'items/show'
 
-  get 'items/edit'
+  # get 'items/edit'
 
-  get 'items/update'
+  # get 'items/update'
 
-  get 'items/destroy'
+  # get 'items/destroy'
 
   resources :items
 
