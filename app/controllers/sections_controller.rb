5317787149e2ffc8ef53a9b3797a271a43c0d74e
Jules Rogmans <websitewizardguy@gmail.com>
2015-04-14 23:44:39 -0700

    Modify seeds.rb, change CSS class for chalk bg

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,5 @@
 class SectionsController < ApplicationController
   def index
-  	@sections = Section.all
   	@items = Item.all.order('updated_at ASC')
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Section.create([{ name: 'To Do', position: 1 }, { name: 'Busy', position: 2 }, { name: 'Done', position: 3 }])
-
 Item.create([
-	{title: 'Accidental', description: 'The committee critiques the ultra offer.', section_id: 1},
-	{title: 'Forthward', description: 'The mist forecasts the condition.', section_id: 1},
-	{title: 'Alopecia', description: 'The son calibrates the kaput crack.', section_id: 1},
-	{title: 'Implicating', description: 'The science formulates the plausible jelly.', section_id: 2},
-	{title: 'Postulation', description: 'The acid pain prioritizes the daughter.', section_id: 2},
-	{title: 'Despisingly', description: 'The ill-fated reason troubleshots the company.', section_id: 2},
-	{title: 'Bouncy', description: 'The touch stimulates the highfalutin son.', section_id: 2},
-	{title: 'Lacertidae', description: 'The untidy snow defers the fruit.', section_id: 3},
-	{title: 'Aviculture', description: 'The transport compiles the join.', section_id: 3},
+	{title: 'Accidental', description: 'The committee critiques the ultra offer.', position: 1},
+	{title: 'Forthward', description: 'The mist forecasts the condition.', position: 1},
+	{title: 'Alopecia', description: 'The son calibrates the kaput crack.', position: 1},
+	{title: 'Implicating', description: 'The science formulates the plausible jelly.', position: 2},
+	{title: 'Postulation', description: 'The acid pain prioritizes the daughter.', position: 2},
+	{title: 'Despisingly', description: 'The ill-fated reason troubleshots the company.', position: 2},
+	{title: 'Bouncy', description: 'The touch stimulates the highfalutin son.', position: 2},
+	{title: 'Lacertidae', description: 'The untidy snow defers the fruit.', position: 3},
+	{title: 'Aviculture', description: 'The transport compiles the join.', position: 3},
 ])
