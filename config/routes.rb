980ce8bf5aca72b79459db82541b0bf1b1ee5101
Jules Rogmans <websitewizardguy@gmail.com>
2015-04-18 16:00:09 -0700

    Change routes root to redirect to /teams

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'teams#index'
+  get '/', to: redirect('/teams')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
